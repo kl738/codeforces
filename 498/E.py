3ae84aa2ba9ee3a71797398ee15d1cd9980cac67
kevin <kl738@cornell.edu>
2018-07-25 00:10:08 -0700

    print statements. TODO: write random large test cases

--- 498/E.py
+++ 498/E.py
@@ -6,6 +6,7 @@ c = [0] * n
 
 for i,v in enumerate(officers):
 	s[v-1].append(i+1)
+print("Sucessors",s)
 
 traversal = []
 def dfs(root):
@@ -16,13 +17,19 @@ def dfs(root):
 		c[root] += c[child]
 
 dfs(0)
+print("Counts",c)
+print("Dfs traversl",traversal)
 
-answer = ""
+
+# answer = ""
 for _ in range(q):
 	u, k = map(int,input().split())
 	start = traversal.index(u-1)
 	if k <= c[start]:
-		answer += (str(traversal[start+k-1]+1) + "\n")
+		# answer += (str(traversal[start+k-1]+1) + "\n")
+		print(str(traversal[start+k-1]+1))
+
 	else:
-		answer += "-1\n"
-print(answer)
+		# answer += "-1\n"
+		print(-1)
+# print(answer)
